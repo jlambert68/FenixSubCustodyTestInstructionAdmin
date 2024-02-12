2fb2792016d95c9c18a1fe8a9ad7543c2fdb1165
jlambert68 <jonas.lambert@drakon.se>
2024-02-12 16:49:12 +0100

    Updated versions

--- TestInstructionsAndTesInstructionContainersAndAllowedUsers/DomainData/DomainData.go
+++ TestInstructionsAndTesInstructionContainersAndAllowedUsers/DomainData/DomainData.go
@@ -6,6 +6,6 @@ import "github.com/jlambert68/FenixTestInstructionsAdminShared/TypeAndStructs"
 const (
 	DomainUUID_SubCustody                 TypeAndStructs.DomainUUIDType = "7edf2269-a8d3-472c-aed6-8cdcc4a8b6ae"
 	DomainName_SubCustody                 TypeAndStructs.DomainNameType = "SubCustody"
-	ExecutionDomainUUID_SubCustody_OnPrem TypeAndStructs.DomainUUIDType = "0e0f68ac-fde0-4c93-b47d-73bfb44a68bd"
+	ExecutionDomainUUID_SubCustody_OnPrem TypeAndStructs.DomainUUIDType = "7edf2269-a8d3-472c-aed6-8cdcc4a8b6ae"
 	ExecutionDomainName_SubCustody_OnPrem TypeAndStructs.DomainNameType = "ExecutionsDoneOnPrem"
 )
