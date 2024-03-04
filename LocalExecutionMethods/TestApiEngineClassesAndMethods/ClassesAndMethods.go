1e2c82058c6e18193cf7bcac3eb68174c40f5723
jlambert68 <jonas.lambert@drakon.se>
2024-03-04 18:25:55 +0100

    Update name for ValidateMT544_v1_0 ValidateMT546_v1_0 ValidateMT548_v1_0

--- LocalExecutionMethods/TestApiEngineClassesAndMethods/ClassesAndMethods.go
+++ LocalExecutionMethods/TestApiEngineClassesAndMethods/ClassesAndMethods.go
@@ -61,23 +61,23 @@ const (
 	TestApiEngine_MethodName_UUID_SubCustody_SendOnMQTypeMT_SendMT542 TestApiEngine_MethodName_UUID_SubCustody_Type = "f6d8a81a-127e-4582-8aaf-e62c4b7c142a"
 	TestApiEngine_MethodName_Name_SubCustody_SendOnMQTypeMT_SendMT542 TestApiEngine_MethodName_Name_SubCustody_Type = "SendMT542_v1_0"
 
-	// ClassName - ***** 'VerifyMQMessageTypeMT' *****
+	// ClassName - ***** 'ValidateMQTypeMT54x' *****
 	TestApiEngine_ClassName_UUID_SubCustody_VerifyMQMessageTypeMT TestApiEngine_ClassName_UUID_SubCustody_Type = "6db25acd-1a2a-471d-a068-62ce01208747"
-	TestApiEngine_ClassName_Name_SubCustody_VerifyMQMessageTypeMT TestApiEngine_ClassName_Name_SubCustody_Type = "VerifyMQMessageTypeMT"
+	TestApiEngine_ClassName_Name_SubCustody_VerifyMQMessageTypeMT TestApiEngine_ClassName_Name_SubCustody_Type = "ValidateMQTypeMT54x"
 
-	// ClassName: 'VerifyMQMessageTypeMT' - MethodName: 'VerifyMT544'
+	// ClassName: 'ValidateMQTypeMT54x' - MethodName: 'ValidateMT544'
 	TestApiEngine_MethodName_UUID_SubCustody_VerifyMQMessageTypeMT_VerifyMT544 TestApiEngine_MethodName_UUID_SubCustody_Type = "09292093-751b-4b41-afd0-8e0a93d49e6a"
 	TestApiEngine_MethodName_Name_SubCustody_VerifyMQMessageTypeMT_VerifyMT544 TestApiEngine_MethodName_Name_SubCustody_Type = "ValidateMT544_v1_0"
 
-	// ClassName:'VerifyMQMessageTypeMT' - MethodName: 'VerifyMT544' - Attributes
+	// ClassName:'ValidateMQTypeMT54x' - MethodName: 'ValidateMT544' - Attributes
 	TestApiEngine_AttributeName_UUID_SubCustody_VerifyMQMessageTypeMT_RelatedReference_54x_20CRELA TestApiEngine_AttributeName_UUID_SubCustody_Type = "9d99a553-23fe-4f02-942f-0d53ddc8acfb"
 	TestApiEngine_AttributeName_Name_SubCustody_VerifyMQMessageTypeMT_RelatedReference_54x_20CRELA TestApiEngine_AttributeName_Name_SubCustody_Type = "RelatedReference_54x_20CRELA"
 
-	// ClassName: 'VerifyMQMessageTypeMT' - MethodName: 'VerifyMT546'
+	// ClassName: 'ValidateMQTypeMT54x' - MethodName: 'ValidateMT546'
 	TestApiEngine_MethodName_UUID_SubCustody_VerifyMQMessageTypeMT_VerifyMT546 TestApiEngine_MethodName_UUID_SubCustody_Type = "db919745-f09b-4afa-9698-c0b9596fc67c"
 	TestApiEngine_MethodName_Name_SubCustody_VerifyMQMessageTypeMT_VerifyMT546 TestApiEngine_MethodName_Name_SubCustody_Type = "ValidateMT546_v1_0"
 
-	// ClassName: 'VerifyMQMessageTypeMT' - MethodName: 'VerifyMT548'
+	// ClassName: 'ValidateMQTypeMT54x' - MethodName: 'ValidateMT548'
 	TestApiEngine_MethodName_UUID_SubCustody_VerifyMQMessageTypeMT_VerifyMT548 TestApiEngine_MethodName_UUID_SubCustody_Type = "f7b0b0da-6b3a-40b8-bdbb-a032a6d8d056"
 	TestApiEngine_MethodName_Name_SubCustody_VerifyMQMessageTypeMT_VerifyMT548 TestApiEngine_MethodName_Name_SubCustody_Type = "ValidateMT58_v1_0"
 )
