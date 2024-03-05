422ada082690fead9ebbb7c5aea40cd57a9bb498
jlambert68 <jonas.lambert@drakon.se>
2024-03-05 10:51:51 +0100

    Fixed bug in TestApiEngine_MethodName_Name_SubCustody_VerifyMQMessageTypeMT_VerifyMT548

--- LocalExecutionMethods/TestApiEngineClassesAndMethods/ClassesAndMethods.go
+++ LocalExecutionMethods/TestApiEngineClassesAndMethods/ClassesAndMethods.go
@@ -79,5 +79,5 @@ const (
 
 	// ClassName: 'ValidateMQTypeMT54x' - MethodName: 'ValidateMT548'
 	TestApiEngine_MethodName_UUID_SubCustody_VerifyMQMessageTypeMT_VerifyMT548 TestApiEngine_MethodName_UUID_SubCustody_Type = "f7b0b0da-6b3a-40b8-bdbb-a032a6d8d056"
-	TestApiEngine_MethodName_Name_SubCustody_VerifyMQMessageTypeMT_VerifyMT548 TestApiEngine_MethodName_Name_SubCustody_Type = "ValidateMT58_v1_0"
+	TestApiEngine_MethodName_Name_SubCustody_VerifyMQMessageTypeMT_VerifyMT548 TestApiEngine_MethodName_Name_SubCustody_Type = "ValidateMT548_v1_0"
 )
