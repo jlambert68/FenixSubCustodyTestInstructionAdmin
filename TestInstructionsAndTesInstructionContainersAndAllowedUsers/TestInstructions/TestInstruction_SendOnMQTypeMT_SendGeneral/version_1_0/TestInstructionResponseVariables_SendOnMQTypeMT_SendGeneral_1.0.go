6266fc05ead701cd9960bae5909b29ab4e8c38c1
jlambert68 <jonas.lambert@drakon.se>
2024-09-23 13:39:27 +0200

    Added json-examples and json-schemas for 'SendOnMQTypeMT_SendGeneral' Added full TI-structure for 'SendOnMQTypeMT_SendGeneral'

--- TestInstructionsAndTesInstructionContainersAndAllowedUsers/TestInstructions/TestInstruction_SendOnMQTypeMT_SendGeneral/version_1_0/TestInstructionResponseVariables_SendOnMQTypeMT_SendGeneral_1.0.go
+++ TestInstructionsAndTesInstructionContainersAndAllowedUsers/TestInstructions/TestInstruction_SendOnMQTypeMT_SendGeneral/version_1_0/TestInstructionResponseVariables_SendOnMQTypeMT_SendGeneral_1.0.go
@@ -1,11 +1,18 @@
 package version_1_0
 
 import (
+	"github.com/jlambert68/FenixSubCustodyTestInstructionAdmin/TestInstructionsAndTesInstructionContainersAndAllowedUsers/TestInstructions"
 	"github.com/jlambert68/FenixTestInstructionsAdminShared/TestInstructionAndTestInstuctionContainerTypes"
 	"github.com/jlambert68/FenixTestInstructionsAdminShared/TypeAndStructs"
+	"github.com/jlambert68/FenixTestInstructionsAdminShared/shared_code"
 )
 
 const ()
+
+const (
+	ResponseVariableUuid_SendOnMQTypeMT_SendGeneral_20CSEME TypeAndStructs.ResponseVariableUuidType = "c002af12-114c-4e55-9574-05fae7cf674c"
+	ResponseVariableName_SendOnMQTypeMT_SendGeneral_20CSEME TypeAndStructs.ResponseVariableNameType = ":20C::SEME//"
+)
 
 // TestInstructionResponseVariables_SendOnMQTypeMT_SendGeneral
 // Variable that holds the data for the TestInstructionResponseVariables
@@ -17,6 +24,20 @@ func Initate_TestInstructionResponseVariables_SendOnMQTypeMT_SendGeneral() {
 
 	// Initiate the TestInstructionResponseVariable
 	TestInstructionResponseVariables_SendOnMQTypeMT_SendGeneral = make(map[TypeAndStructs.ResponseVariableUuidType]*TestInstructionAndTestInstuctionContainerTypes.ResponseVariableStructureStruct)
+
+	TestInstructionResponseVariables_SendOnMQTypeMT_SendGeneral = map[TypeAndStructs.ResponseVariableUuidType]*TestInstructionAndTestInstuctionContainerTypes.ResponseVariableStructureStruct{
+		ResponseVariableUuid_SendOnMQTypeMT_SendGeneral_20CSEME: &TestInstructionAndTestInstuctionContainerTypes.ResponseVariableStructureStruct{
+			ResponseVariable: TypeAndStructs.ResponseVariableStruct{
+				ResponseVariableUuid:        ResponseVariableUuid_SendOnMQTypeMT_SendGeneral_20CSEME,
+				ResponseVariableName:        ResponseVariableName_SendOnMQTypeMT_SendGeneral_20CSEME,
+				ResponseVariableDescription: TypeAndStructs.ResponseVariableDescriptionType("An identifier for the SendOnMQTypeMT_SendGeneral-message"),
+				ResponseVariableIsMandatory: TypeAndStructs.ResponseVariableIsMandatoryType(true),
+				ResponseVariableTypeUuid:    TestInstructions.ResponseVariableTypeUuid_54x_20CSEME,
+				ResponseVariableTypeName:    TestInstructions.ResponseVariableTypeName_54x_20CSEME,
+			},
+			ResponseVariableHash: shared_code.InitialValueBeforeHashed,
+		},
+	}
 
 	return
 }
