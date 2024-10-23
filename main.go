b1a915f4ecfaf6ba3db9721d72cae88fbe3e4959
jlambert68 <jonas.lambert@drakon.se>
2024-10-23 16:12:58 +0200

    Public Key is printed when run

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/jlambert68/FenixSubCustodyTestInstructionAdmin/TestInstructionsAndTesInstructionContainersAndAllowedUsers"
 	"github.com/jlambert68/FenixSyncShared/environmentVariables"
+	"github.com/jlambert68/FenixTestInstructionsAdminShared/shared_code"
+	"log"
 )
 
 //go:embed TestInstructionsAndTesInstructionContainersAndAllowedUsers/allowedUsers/allowedUsers.json
@@ -23,4 +25,13 @@ func main() {
 	fmt.Println(tempTestApiEngineClassesAndMethodsAndAttributes)
 	fmt.Println(TestInstructionsAndTesInstructionContainersAndAllowedUsers.TestInstructionsAndTestInstructionContainersAndAllowedUsers_SubCustody)
 	fmt.Println("Success when generating and verifying all messages for TestInstructions, TestInstructionContainers and AllowedUsers")
+
+	var publicKey string
+	var err error
+	publicKey, err = shared_code.GeneratePublicKeyAsBase64StringFromPrivateKey()
+	if err != nil {
+		log.Fatalln("Couldn't generate 'Public Key' from 'Private Key'")
+	}
+
+	fmt.Println(fmt.Sprintf("Public Key = %s", publicKey))
 }
