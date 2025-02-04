d32ff54580af031ea8dff9c4094f9610cd80e7f1
jlambert68 <jonas.lambert@drakon.se>
2025-02-04 11:50:35 +0100

    Start adding FenixGeneral_SendTestDataToThisDomain_1_0 to support TestApiEngine-calls, v3

--- TestInstructionsAndTesInstructionContainersAndAllowedUsers/TestInstructions/TestInstruction_FenixGeneral_SendTestDataToThisDomain/version_1_0/TestInstruction_FenixGeneral_SendTestDataToThisDomain_1.0.go
+++ TestInstructionsAndTesInstructionContainersAndAllowedUsers/TestInstructions/TestInstruction_FenixGeneral_SendTestDataToThisDomain/version_1_0/TestInstruction_FenixGeneral_SendTestDataToThisDomain_1.0.go
@@ -5,9 +5,21 @@ import (
 	"github.com/jlambert68/FenixSubCustodyTestInstructionAdmin/LocalExecutionMethods"
 	"github.com/jlambert68/FenixSubCustodyTestInstructionAdmin/LocalExecutionMethods/TestApiEngineClassesAndMethods"
 	"github.com/jlambert68/FenixSubCustodyTestInstructionAdmin/TestInstructionsAndTesInstructionContainersAndAllowedUsers/TestInstructions"
+	"github.com/jlambert68/FenixTestInstructionsAdminShared/Domains"
 	"github.com/jlambert68/FenixTestInstructionsAdminShared/TestInstructionAndTestInstuctionContainerTypes"
 	"github.com/jlambert68/FenixTestInstructionsAdminShared/TypeAndStructs"
 	"strconv"
+)
+
+const (
+	// Attribute - 'ExpectedToBePassed'
+	TestInstructionAttributeUUID_FenixGeneral_SendTestDataToThisDomain_ExpectedToBePassed               TypeAndStructs.TestInstructionAttributeUUIDType = "5d354d15-5612-4e83-a0a3-98654f21f989" //TestInstructionAttributeUUID_SubCustody_ExpectedToBePassed
+	TestInstructionAttributeName_FenixGeneral_SendTestDataToThisDomain_ExpectedToBePassed               TypeAndStructs.TestInstructionAttributeNameType = TestInstructions.TestInstructionAttributeName_SubCustody_ExpectedToBePassed
+	TestInstructionAttributeType_FenixGeneral_SendTestDataToThisDomain_ExpectedToBePassed               TypeAndStructs.TestInstructionAttributeTypeType = TestInstructions.TestInstructionAttributeType_SubCustody_ExpectedToBePassed
+	TestInstructionAttributeValueAsStringValue_FenixGeneral_SendTestDataToThisDomain_ExpectedToBePassed TypeAndStructs.AttributeValueAsStringType       = Domains.TestInstructionAttributeValueAsStringValue_TRUE
+	TestInstructionAttributeValueUUID_FenixGeneral_SendTestDataToThisDomain_ExpectedToBePassed          TypeAndStructs.AttributeValueUUIDType           = Domains.TestInstructionAttributeValueUUID_TRUE
+	TestInstructionAttributeDescription_FenixGeneral_SendTestDataToThisDomain_ExpectedToBePassed        string                                          = "Should the TestInstruction execution to be expected to succeed or not"
+	TestInstructionAttributeMouseOverText_FenixGeneral_SendTestDataToThisDomain_ExpectedToBePassed      string                                          = "Should the TestInstruction execution to be expected to succeed or not"
 )
 
 // TestInstruction_FenixGeneral_SendTestDataToThisDomain
@@ -76,18 +88,31 @@ func Initate_TestInstruction_FenixGeneral_SendTestDataToThisDomain() *TestInstru
 	// Add TestApiEngine relation for Attribute - 'ExpectedToBePassed'
 	var tempTestApiEngineAttributeExpectedToBePassed *TestApiEngineClassesAndMethodsAndAttributes.TestApiEngineAttributesStruct
 	tempTestApiEngineAttributeExpectedToBePassed = &TestApiEngineClassesAndMethodsAndAttributes.TestApiEngineAttributesStruct{
-		TestInstructionAttributeUUID: TestInstruction_FenixGeneral_SendTestDataToThisDomain_v1_0.
-			TestInstructionAttributeUUID_FenixSentToUsersDomain_SendTestDataToThisDomain_SendTestDataToThisExecutionDomain,
-		TestInstructionAttributeName: TestInstruction_FenixGeneral_SendTestDataToThisDomain_v1_0.
-			TestInstructionAttributeName_FenixSentToUsersDomain_SendTestDataToThisDomain_SendTestDataToThisExecutionDomain,
+		TestInstructionAttributeUUID:         TestInstructionAttributeUUID_FenixGeneral_SendTestDataToThisDomain_ExpectedToBePassed,
+		TestInstructionAttributeName:         TestInstructionAttributeName_FenixGeneral_SendTestDataToThisDomain_ExpectedToBePassed,
 		TestInstructionAttributeTypeUUID:     TestInstructions.TestInstructionAttributeTypeUUID_SubCustody_ExpectedToPass,
 		TestApiEngineAttributeNameUUID:       TestApiEngineClassesAndMethodsAndAttributes.TestApiEngine_ClassName_UUID_SubCustody_GeneralAttribute_ExpectedToBePassed,
 		TestApiEngineAttributeNameName:       TestApiEngineClassesAndMethodsAndAttributes.TestApiEngine_ClassName_Name_SubCustody_GeneralAttribute_ExpectedToBePassed,
 		AttributeShouldBeSentToTestApiEngine: true,
 	}
 
+	testApiEngineMethodAttributeMap[TestInstructionAttributeUUID_FenixGeneral_SendTestDataToThisDomain_ExpectedToBePassed] = tempTestApiEngineAttributeExpectedToBePassed
+
+	// Add TestApiEngine relation for Attribute - 'ChosenTestDataAsJsonString'
+	var tempTestApiEngineAttributeChosenTestDataAsJsonString *TestApiEngineClassesAndMethodsAndAttributes.TestApiEngineAttributesStruct
+	tempTestApiEngineAttributeChosenTestDataAsJsonString = &TestApiEngineClassesAndMethodsAndAttributes.TestApiEngineAttributesStruct{
+		TestInstructionAttributeUUID: TestInstruction_FenixGeneral_SendTestDataToThisDomain_v1_0.
+			TestInstructionAttributeUUID_SendTestDataToThisDomain_ChosenTestDataAsJsonString,
+		TestInstructionAttributeName: TestInstruction_FenixGeneral_SendTestDataToThisDomain_v1_0.
+			TestInstructionAttributeName_SendTestDataToThisDomain_ChosenTestDataAsJsonString,
+		TestInstructionAttributeTypeUUID:     TestInstructions.TestInstructionAttributeTypeUUID_SubCustody_Standard,
+		TestApiEngineAttributeNameUUID:       TestApiEngineClassesAndMethodsAndAttributes.TestApiEngine_AttributeName_UUID_FenixGeneral_SendTestDataToThisDomain_ChosenTestDataAsJsonString,
+		TestApiEngineAttributeNameName:       TestApiEngineClassesAndMethodsAndAttributes.TestApiEngine_AttributeName_Name_SFenixGeneral_SendTestDataToThisDomain_ChosenTestDataAsJsonString,
+		AttributeShouldBeSentToTestApiEngine: true,
+	}
+
 	testApiEngineMethodAttributeMap[TestInstruction_FenixGeneral_SendTestDataToThisDomain_v1_0.
-		TestInstructionAttributeUUID_FenixSentToUsersDomain_SendTestDataToThisDomain_SendTestDataToThisExecutionDomain] = tempTestApiEngineAttributeExpectedToBePassed
+		TestInstructionAttributeUUID_FenixSentToUsersDomain_SendTestDataToThisDomain_SendTestDataToThisExecutionDomain] = tempTestApiEngineAttributeChosenTestDataAsJsonString
 
 	return TestInstruction_FenixGeneral_SendTestDataToThisDomain
 }
